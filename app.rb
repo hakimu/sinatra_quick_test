bf6eb605be4382bd60d6d34ef20db532af845455
Hakimu Jackson <hakimu@newrelic.com>
2015-08-27 10:27:23 -0700

    Updated app.rb and added config

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require 'sinatra'
 require 'net/http'
 require 'newrelic_rpm'
 
-newrelic_ignore '/front'
+# newrelic_ignore '/front'
+
+get '/test/:name' do
+	"Hello #{params['name']}"
+end
 
 get '/hi' do
 	#{"foo" => "bar", "one" => "two"}
@@ -24,44 +28,10 @@ get '/front' do
 end
 
 get '/back' do
-	sleep 3
 	new_hash = {"message" => "hello world"}
 	new_json = JSON.generate(new_hash)
 	new_json 
 end
-# set :port, 9311
-
-# get '/' do
-# 	sleep 3
-# 	'Hello world'
-# end
-
-# get '/test' do
-# 	uri = URI('http://localhost:4567/test')
-# 	100.times do 
-# 		Net::HTTP.get(uri)
-# 		print uri
-# 	end
-# end
-
-# get '/test' do
-# 	'testing'
-# end
-
-# get '/custom' do
-# 	100.times do
-# 	  number = rand(5)
-# 	  ::NewRelic::Agent.record_metric('Custom/test/alert', number)
-# 	  sleep(2)
-# 	  puts Process.ppid
-# 	  puts Process.pid
-# 	end
-# end
-
-# get '/map' do
-# 	uri = URI('http://127.0.0.1:4567')
-# 	Net::HTTP.get(uri)
-# end
 
 get '/external' do
 	uri = URI('http://www.usatoday.com')
